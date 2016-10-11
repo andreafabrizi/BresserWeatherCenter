c95e106f34684b72ea4fb75d386fbd2c55879c57
Andrea Fabrizi <andrea.fabrizi@gmail.com>
2016-10-11 15:41:34 +0200

    Updated test script

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# rtl_fm -M am -f 868.300M -s 48k -g 50 -l 15  | ./test.py                                                                                                                                                                                                                      Displayed daily on lcd        
+# rtl_fm -M am -f 868.300M -s 48k -g 50 | ./test.py                                                                                                                                                                                                                      Displayed daily on lcd        
 #
 #
 from bresser import *
@@ -9,13 +9,14 @@ def process_packet(p):
                         
     print "Humidity: %d%% " % p.getHumidity(),
     print "Temperature: %.1f" % p.getTemperature() + u"\u00b0 ",
-    print "Wind: %.1f m/s " % p.getWindSpeed(),
+    print "Wind: %.1f m/s %s" % (p.getWindSpeed(), p.getWindDirection()),
     print "Rain: %.1f mm" % p.getRain(),
     print ""
  
 if __name__ == "__main__":
 
-    b = Bresser()
+    #Noise neede to be adjusted manually
+    b = Bresser(noise = 700)
     b.set_callback(process_packet)
     b.process_radio_data()
 
